ba51a7f0c98e92bebd42204b7c6a3df904a6df8a
Taylor C. Richberger <Taywee@gmx.com>
2016-05-31 18:21:48 -0600

    improve logic

--- main.cxx
+++ main.cxx
@@ -1,27 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <list>
 #include <sstream>
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
-
-// split a string into an output iterator of some sort.
-template <typename OutputIterator>
-void split(const std::string &source, const std::string &delimiter, OutputIterator result)
-{
-    size_t prevposition = 0;
-    size_t position = 0;
-    while ((position < source.size()) && ((position = source.find(delimiter, position)) != source.npos))
-    {
-        *result = source.substr(prevposition, position - prevposition);
-        result++;
-        position += delimiter.size();
-        prevposition = position;
-    }
-    *result = source.substr(prevposition);
-    result++;
-}
 
 struct OID
 {
@@ -30,28 +14,39 @@ struct OID
     std::string data;
 };
 
+static std::list<std::string> split(const std::string &source, const std::string &delimiter);
 static std::istream &operator>>(std::istream &stream, OID &oid);
 
 int main()
 {
-    init_snmp("snmpapp");
-    while (true)
+    init_snmp("snmpwalktranslate");
+    OID oid;
+    std::array<char, 4096> buffer;
+    while (std::cin >> oid)
     {
-        OID oid;
-        std::cin >> oid;
-        if (std::cin.eof())
-        {
-            break;
-        }
-        std::vector<char> oidstring;
-        oidstring.resize(2024, '\0');
-        snprint_objid(oidstring.data(), oidstring.size(), oid.id.data(), oid.id.size());
-        std::cout << oidstring.data() << " = " << oid.data << '\n';
+        const std::size_t length = snprint_objid(buffer.data(), buffer.size(), oid.id.data(), oid.id.size());
+        const std::string oidstring(buffer.data(), length);
+        std::cout << oidstring << " = " << oid.data << '\n';
     }
 
     return (0);
 }
 
+std::list<std::string> split(const std::string &source, const std::string &delimiter)
+{
+    std::list<std::string> output;
+    size_t prevposition = 0;
+    size_t position = 0;
+    while ((position < source.size()) && ((position = source.find(delimiter, position)) != source.npos))
+    {
+        output.emplace_back(source, prevposition, position - prevposition);
+        position += delimiter.size();
+        prevposition = position;
+    }
+    output.emplace_back(source, prevposition, position - prevposition);
+
+    return output;
+}
 
 std::istream &operator>>(std::istream &stream, OID &oid)
 {
@@ -62,8 +57,7 @@ std::istream &operator>>(std::istream &stream, OID &oid)
     std::getline(stream, oid.data);
 
     oid.id.clear();
-    std::list<std::string> splitOid;
-    split(oid.stringOid, ".", std::back_inserter(splitOid));
+    std::list<std::string> splitOid(split(oid.stringOid, "."));
 
     // Remove the first blank field, caused by the leading dot
     splitOid.pop_front();
